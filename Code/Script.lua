a529017980c70462aef04d45708dcbe5199599b0
Andreas Pardeike <andreas@pardeike.net>
2024-01-11 20:01:00 +0100

    v1.1 fixes selection, adds 'Target Areas' button

--- Code/Script.lua
+++ Code/Script.lua
@@ -1,3 +1,49 @@
+local otherTurrets = {}
+
+local originalTargetAreaSet = TargetAreaObject.TargetAreaSet
+function TargetAreaObject:TargetAreaSet(area_state)
+	originalTargetAreaSet(self, area_state)
+	if #otherTurrets > 0 then
+		local center = area_state.pos
+		for i = 2, #otherTurrets do
+			local obj = otherTurrets[i]
+			obj.target_area_center = center
+			obj:OnAttackTargetChanged()
+			obj:ShowRanges()
+		end
+		otherTurrets = {}
+	end
+end
+
+local originalCreateBinActions = MultiSelectAdapter.CreateBinActions
+function MultiSelectAdapter:CreateBinActions(bin, host)
+	originalCreateBinActions(self, bin, host)
+	if #bin > 1 and IsKindOf(bin[1], "TargetAreaObject") then
+		XAction:new({
+			ActionId = "BrrainzTargetArea",
+			ActionSortKey = "2000",
+			ActionIcon = "UI/Icons/Infopanels/attack",
+			ActionToolbar = "ip_actions",
+			RolloverTemplate = "Rollover",
+			ActionState = function(self, host)
+				self.ActionIcon = "UI/Icons/Infopanels/attack"
+				self.ActionName = "Target Areas"
+				self.RolloverText = "Choose Target Areas."
+				self.RolloverTitle = "Target Areas"
+				self.RolloverHint = ""
+			end,
+			OnAction = function()
+				for i = 1, #bin do
+					otherTurrets[#otherTurrets + 1] = bin[i]
+				end
+				bin[1]:TargetAreaPlace()
+			end
+		}, host)
+	end
+end
+
+-- add a new shortcut to the game
+
 PlaceObj("XTemplate", {
 	group = "Shortcuts",
 	id = "BrrainzShortcuts",
@@ -25,10 +71,12 @@ PlaceObj("XTemplate", {
 			local center = Selection[1].target_area_center
 			if center then
 				for i = 2, #Selection do
-					Selection[i].target_area_center = center
+					local obj = Selection[i]
+					obj.target_area_center = center
+					obj:OnAttackTargetChanged()
+					obj:ShowRanges()
 				end
 			end
-			SelectObj(nil)
 		end,
 		"__condition",
 		function(parent, context)
@@ -38,5 +86,4 @@ PlaceObj("XTemplate", {
 		true
 	})
 })
-
 XTemplateSpawn("BrrainzShortcuts", XShortcutsTarget)
